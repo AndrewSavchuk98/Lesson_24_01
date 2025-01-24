70bb449a4c9e5108e3dc0d15e099f3b4eb7c95e9
student <andrej.petrenko.110@gmail.com>
2025-01-24 19:32:23 +0200

    Tasks

--- Lesson_24_01/Program.cs
+++ Lesson_24_01/Program.cs
@@ -58,7 +58,7 @@ namespace Lesson_24_01
             }
             System.Console.WriteLine(c);*/
 
-            int[] array = { 1, 5, 5, 8, 10 };
+           /* int[] array = { 1, 5, 5, 8, 10 };
 
             System.Console.WriteLine("Size " + array.Length);
 
@@ -69,7 +69,7 @@ namespace Lesson_24_01
                 System.Console.Write(array[i] + " ");
 
             }
-
+*/
 
            /* int j = 0;
             while (j < 100)
@@ -87,7 +87,7 @@ namespace Lesson_24_01
             // a % 2 == 0
 
             // Відсортувати масив
-
+/*
             int max = array[0];
             int min = array[0];
             int sum = 0;
@@ -136,6 +136,84 @@ namespace Lesson_24_01
 
             }
             System.Console.WriteLine();
+
+            foreach (int item in array)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            // { 0, 0, 0} 0 0
+            // 0 0 0
+            // 0 0 0
+
+            int[,] doubleArray = { { 0, 0, 0}, { 5, 8, 7}, { 1, 2, 4} };
+
+            System.Console.WriteLine(doubleArray[1, 1]);
+*//*
+            for (int i = 0; i < doubleArray.Length; i++)
+            {
+                System.Console.Write(doubleArray[0, i] + " ");
+            }*//*
+
+            for (int i = 0; i < doubleArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < doubleArray.GetLength(1); j++)
+                {
+                    System.Console.Write(doubleArray[i, j] + " ");
+                }
+                System.Console.WriteLine();
+            }
+
+            string text = "Hello";
+            foreach (char item in text)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            Random random = new Random();
+
+            string[] te = { "a" };
+            int result  = random.Next(1, 100);*/
+
+            //using System.Linq;
+
+            //            "12354".Split(); -> { "1", "2", "3", "4", "5"}
+
+            // res += i   -> 54321
+
+            // Task1 Програма загадує випадкове число від 1 до 100, користувач намагається вгадати його.
+            // Програма виводить підсказки Більше чи Менше
+
+            // Task 2 Виведіть таблицю множення у вигляді матриці (таблиці)
+            // 1 2 3  
+            // 2 4 6
+            // 3 6 9  ...
+
+            // && - and
+            // || - or
+
+            // Task 3 Користувач вводить рядок, а програма рахує кільтість голосних в цьому рядку (а, е, є, і, ї, о, у, я)
+
+            // Task 4 Знайти суму чисел в заданому діапаозоні який вводить користувач
+            // Наприклад користувач ввів 5 і 8  (5 + 6 + 7 + 8) = 26, якщо користувач вводить однаковий діапазон, наприклад 10 і 10
+            // то попросити його ввести новий
+
+            // Task 5. Реверс числа.  користувач вводить число 584921 отримуємо 129485
+
+            string numberText = "584921";
+
+            /*string[] numbers = numberText.Split();*/
+
+            //System.Console.WriteLine(numbers.Length);
+            string newNumberText = "";
+            for (int i = numberText.Length - 1; i >= 0; i--)
+            {
+                System.Console.Write(numberText[i]);
+                
+            }
+
+        
+
         }
     }
 }
